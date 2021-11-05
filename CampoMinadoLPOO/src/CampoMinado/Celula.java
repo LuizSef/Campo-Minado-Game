434dd88aca71113f35cfa476906b27ccfc5f2827
LucasRomero03 <88542920+LucasRomero03@users.noreply.github.com>
2021-11-05 01:23:35 -0300

    Classe tabuleiro
    implementação da classe tabuleiro e criação de método para iniciar o tabuleiro .

--- CampoMinadoLPOO/src/CampoMinado/Celula.java
+++ CampoMinadoLPOO/src/CampoMinado/Celula.java
@@ -1,4 +1,4 @@
-package CampoMinado;
+ package CampoMinado;
 
 import java.util.ArrayList;
 
--- CampoMinadoLPOO/src/CampoMinado/Tabuleiro.java
+++ CampoMinadoLPOO/src/CampoMinado/Tabuleiro.java
@@ -1,5 +1,40 @@
 package CampoMinado;
 
 public class Tabuleiro {
+	 private int nLinhas = 9;
+	 private int nColunas = 9 ;
+	 
+	Celula[][] campo = new Celula[nLinhas][nColunas];
+	
 
+	
+	
+	public Tabuleiro( ) {
+		
+		
+		
+		
+	}
+	
+	
+	
+	public void IniciarTabuleiro() {
+		for (int l = 0; l < nLinhas; l++) {
+			for (int c = 0; c < nColunas; c++) {
+				campo[l][c] = new Celula();
+			}
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
